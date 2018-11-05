505edddee5ad6b36438608d5b46a47e3bf25daa4
Hannah Kang <kyouyatamax@gmail.com>
2018-11-05 15:03:03 -0800

    Skeleton HTML with dummy buttons set up

--- main.js
+++ main.js
@@ -1,7 +1,27 @@
 // JS code that checks validation as the user progresses the form
 
+var form = document.getElementsByTagName('form')[0];
+console.log(form);
+// For future note: I could make a function to get all the element
+// Ids inside the formso that I don't have to explicitly type this all out
+// Thats a future feature to implement.
+var email =document.getElementById('email');
+var emailConfirm =document.getElementById('emailConfirm');
+var country =document.getElementById('country');
+var zipCode =document.getElementById('zipCode');
+var pw = document.getElementById('pw');
+var pwConfirm = document.getElementById('pwConfirm');
 // If user leaves form field, it should automatically validate the field
-function whatth() {
-  var helloWorld = "hello world";
-  console.log(helloWorld);
+// function whatth() {
+//   var helloWorld = "hello world";
+//   console.log(helloWorld);
+// }
+
+
+// the submit button should doublecheck and make sure all fields
+//  are validly filled in. 
+var pwValue = pw.value;
+var pwConfirmValue = pwConfirm.nodeValue;
+if (pwConfirmValue!=pwValue){
+  console.log("the passwords don't match blergh")
 }
