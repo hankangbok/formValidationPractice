7369c7f488b80d8b7ac075357301a67ed0c1ae48
Hannah Kang <kyouyatamax@gmail.com>
2018-11-05 14:34:47 -0800

    Blank initial commit

---
+++ main.js
@@ -0,0 +1,7 @@
+// JS code that checks validation as the user progresses the form
+
+// If user leaves form field, it should automatically validate the field
+function whatth() {
+  var helloWorld = "hello world";
+  console.log(helloWorld);
+}
