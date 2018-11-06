065b96f6f280abbc8b3760001c2b6dd16f892e1c
Hannah Kang <kyouyatamax@gmail.com>
2018-11-05 18:57:23 -0800

    Everything seems functional! Time for more testing

--- main.js
+++ main.js
@@ -1,7 +1,6 @@
 // JS code that checks validation as the user progresses the form
 
 var form = document.getElementsByTagName('form')[0];
-console.log(form);
 // For future note: I could make a function to get all the element
 // Ids inside the formso that I don't have to explicitly type this all out
 // Thats a future feature to implement.
@@ -12,28 +11,35 @@ let zipCode = document.getElementById('zipCode');
 let zipCodeExtension = document.getElementById('zipCodeExtension');
 let pw = document.getElementById('pw');
 let pwConfirm = document.getElementById('pwConfirm');
-// When user leaves form field, it should automatically validate the field
 
 // Email match check
-if (emailConfirm.value != email) {
-  console.log("the emails dont match, add error");
+// Password match check
+function validateConfirmisEqual(object1, object2) {
+  if (object1.value != object2.value) {
+    console.log(object1.id +" does not match " + object2.id);
+    object2.className = ":invalid";
+    object2.setCustomValidity(object2 + ' doesn\'t match');
+  }
+  if (object1.value==object2.value) {
+    object2.setCustomValidity('');
+    object2.className= ":valid";
+  }
 }
 
-// Country is dropdown, so just need to make sure a country is selected.
-// Zipcode valid Regex Check
-// rule1 --> 5 digit zip
-// rule2 -->zips can also be 5 digits (dash) and extra 4 digits
-// rule1 = /[0-9]/;
+// Zipcode Validation
 function validateZip(zipObject) {
-  let rule = new RegExp('[0-9]{5}');
+  let rule = new RegExp('^[0-9]{5}$');
   // also add rule that allows for dash-preceded 4 digit extension
-  let rule = new RegExp('[0-9]{5}');
+  let rule2 = new RegExp('^[0-9]{5}(-[0-9]{4})$');
   let checkZip = rule.test(zipCode.value);
-  if (checkZip!=true) {
+  let checkZip2 = rule2.test(zipCode.value);
+  if (checkZip!=true && checkZip2!=true) {
     console.log("theres no valid zip");
+    zipObject.setCustomValidity('That is not a valid zip');
   }
-  if (checkZip==true) {
-    console.log("the zip is ok")
+  else {
+    console.log("the zip is ok");
+    zipObject.setCustomValidity('');
   }  
 }
 // Valid Password Check
@@ -41,20 +47,23 @@ function validateZip(zipObject) {
 // check for at least 1 uppercase
 // check for at least 1 non-alphanumeric symbol
 function validatePW(pwObject) {
-  let rule=
-  let 
-  pwObject = 
-}
-
-// Password Match Check
-let pwValue = pw.value;
-let pwConfirmValue = pwConfirm.nodeValue;
-function validateConfirmisEqual(object1, object2) {
-  if (object1.value != object2.value) {
-    console.log(object1.id +" does not match " + object2.id);
+  // let rulePW = new RegExp('^(?=.*\d)(?=.*[A-Z])(?=.*\W)$');
+  let rulePW = /(?=.*\d)(?=.*[A-Z])(?=.*\W)/;
+  let checkPW = rulePW.test(pwObject.value);
+  if (checkPW==false) {
+    console.log(pwObject.value);
+    console.log("this password is invalid");
+    pwObject.classList.add('invalid');
+    pwObject.setCustomValidity('invalid!');
+  }
+  if (checkPW==true) {
+    console.log(pwObject.value);
+    console.log("that pw is acceptable")
+    pwObject.setCustomValidity('');
   }
 }
 
-
-// the submit button should doublecheck and make sure all fields
-//  are validly filled in. 
+document.querySelector('#submit').addEventListener("click",function(e){
+  alert("that worked! High 5")
+  e.preventDefault();
+});
